7a3b2ea5bacc49f7c4175e2826b331a0f8545373
SphurtiD <sphurtirdixit09@gmail.com>
2025-03-08 00:46:26 +0530

    Fix ESLint unused variables error

--- app/actions.ts
+++ app/actions.ts
@@ -1,6 +1,4 @@
 "use server";
-
-import { get } from "http";
 import { prisma } from "./utils/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
